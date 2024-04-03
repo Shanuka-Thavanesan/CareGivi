de8e6dd1563b15df1bc77be54df4a891a9ce70fe
Shanuka <shanukathavanesan@gmail.com>
2024-04-03 11:33:11 +0530

    hosting

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,4 @@
-export const BASE_URL ='http://localhost:5000/api/v1';
+export const BASE_URL ='https://caregivi.onrender.com/api/v1';
 export const token = localStorage.getItem("token");
+
+// http://localhost:5000/api/v1
